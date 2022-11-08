317ccfd5f0bde0c9a30f1f54aefe543d55ced995
Rodrigo Orlando <115408313+rodrigod3vops@users.noreply.github.com>
2022-11-08 19:36:54 -0300

    Add files via upload

---
+++ ModuloPrincipal.py
@@ -0,0 +1,21 @@
+from MODULO_LISTA_EM_LISTA.LISTAR_DEL_CONS_MODULOS import *
+
+minhaLista=[]
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista)
+
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+
+print("Resumindo")
+resumirValores(minhaLista)
+
+
